be979d6604b80dadcd97abf7054fb8deef0906a6
Asaad Budayr <asad_289@hotmail.com>
2019-04-11 05:43:15 -0700

    Feat: Fetch initial data

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { handleInitialData } from '../actions/shared'
+import { connect } from 'react-redux'
+class App extends Component {
+  componentDidMount () {
+    this.props.dispatch(handleInitialData());
+  }
+  render() {
+    return (
+      <div className="App">
+      Hello world
+      </div>
+    );
+  }
+}
+
+export default connect()(App);
