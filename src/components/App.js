ea8e87d918865188ccd8d2ccf9de823eb8487858
Asaad Budayr <asad_289@hotmail.com>
2019-04-12 13:37:50 -0700

    Feat: Add loading bar

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import "../App.css";
 import { handleInitialData } from "../actions/shared";
 import { connect } from "react-redux";
 import Dashboard from "./Dashboard";
+import LoadingBar from 'react-redux-loading';
+
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
     return (
-      <div className="App">
+      <div>
+        <LoadingBar />
         {this.props.loading === true ? null : <Dashboard />}
       </div>
     );
