782bf77b63d330cdd7a0aef26ef068a6853dfbb1
Asaad Budayr <asad_289@hotmail.com>
2019-04-19 11:50:17 -0700

    Feat: Create PrivateRoute to handle authentication

--- src/components/App.js
+++ src/components/App.js
@@ -8,23 +8,29 @@ import LoadingBar from "react-redux-loading";
 import NewTweet from "./NewTweet";
 import Nav from "./Nav";
 import TweetPage from './TweetPage'
+import PrivateRoute from './PrivateRoute'
+import Login from './Login'
+import NotFound from './NotFound'
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
+    const { authedUser } = this.props;
     return (
       <Router>
         <Fragment>
           <LoadingBar />
           <div className="container">
-            <Nav />
+            {authedUser && <Nav />}
             {this.props.loading === true ? null : (
               <Switch>
-                <Route path="/" exact component={Dashboard} />
-                <Route path="/new" exact component={NewTweet} />
-                <Route path="/tweet/:id" exact component={TweetPage} />
+                <Route path="/" exact component={Login} />
+                <PrivateRoute path="/home" exact component={Dashboard} />
+                <PrivateRoute path="/new" exact component={NewTweet} />
+                <PrivateRoute path="/tweet/:id" exact component={TweetPage} />
+                <Route component={NotFound} />
               </Switch>
             )}
           </div>
@@ -34,9 +40,10 @@ class App extends Component {
   }
 }
 
-function mapStateToProps({ authedUser }) {
+function mapStateToProps({ authedUser, users }) {
   return {
-    loading: authedUser === null
+    authedUser,
+    loading: !users === null
   };
 }
 export default connect(mapStateToProps)(App);
