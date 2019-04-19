92125a04cb00ab6f0b5d0c289a53afdb1b115844
Asaad Budayr <asad_289@hotmail.com>
2019-04-19 11:57:04 -0700

    Feat: Add a logout button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,49 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { resetAuthedUser } from "../actions/authedUser";
+import { NavLink, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default function Nav () {
-  return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/new' activeClassName='active'>
-            New Tweet
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+class Nav extends React.Component {
+  handleLogout = event => {
+    console.log(this.props);
+    event.preventDefault();
+    this.props.history.push("/");
+    const { dispatch, authedUser } = this.props;
+    dispatch(resetAuthedUser(authedUser));
+  };
+  handleLogout = event => {
+    console.log(this.props);
+    event.preventDefault();
+    this.props.history.push("/");
+    const { dispatch, authedUser } = this.props;
+    dispatch(resetAuthedUser(authedUser));
+  };
+  render(){
+    return (
+      <nav className='nav'>
+        <ul>
+          <li>
+            <NavLink to='/home' exact activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/new' activeClassName='active'>
+              New Tweet
+            </NavLink>
+          </li>
+          <li className="last">
+            <input type="button" color="inherit" value="Logout" onClick={this.handleLogout} />
+          </li>
+        </ul>
+      </nav>
+    )
+  }
 }
+
+function mapStateToProps({ authedUser, users }) {
+  return {
+    authedUser,
+  };
+}
+export default withRouter(connect(mapStateToProps)(Nav));
